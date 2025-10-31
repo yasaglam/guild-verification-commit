ad986584fb854e888fde3418437a15a368db508b
Yalper Sag <59878950+yasaglam@users.noreply.github.com>
2025-10-31 15:03:15 +0100

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-10-31T14:03:13.390Z
-Commit: Fix broken links in documentation
+Timestamp: 2025-10-31T14:03:15.485Z
+Commit: Patch security vulnerability in authentication
