0b105b4ec05588ef4171dd3dc355b3e1426589b6
Yalper Sag <59878950+yasaglam@users.noreply.github.com>
2025-10-31 15:03:12 +0100

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-10-31T14:03:10.826Z
-Commit: Add export functionality for reports
+Timestamp: 2025-10-31T14:03:13.390Z
+Commit: Fix broken links in documentation
