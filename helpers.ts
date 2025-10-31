7db47fa9163285578fc0851080e568cb2e3cbae3
Yalper Sag <59878950+yasaglam@users.noreply.github.com>
2025-10-31 15:03:18 +0100

    Resolve timezone issues in date handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-10-31T14:03:15.485Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2025-10-31T14:03:18.717Z
+Commit: Resolve timezone issues in date handling
